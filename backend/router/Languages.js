6e5cfc0f71a5a6cbabce4d0a90568d5d8e5204d1
mijeong lee <lmjing1234@gmail.com>
2017-09-30 18:09:02 +0000

    언어 목록 조회 구현

--- backend/router/Languages.js
+++ backend/router/Languages.js
@@ -4,11 +4,35 @@ var router = express.Router();
 var Language = require('../model/Language');
 
 router.post('/', function(req, res) {
-  Language.create({
+  var language = new Language({
     language: req.body.language
-  }, function(err, language) {
-    if (err) return res.status(500).send('Language 생성 실패');
-    return res.status(200).send(language);
+  });
+  language.save()
+  .then((language) => {
+    return res.status(201).json(language);
+  })
+  .catch((err) => {
+  console.log(err);
+    if (err.name == 'ValidationError') {
+      return res.status(400).json('invalid input, object invalid.');
+    }else if (err.name == 'MongoError') {
+      return res.status(409).json('an existing item already exists');
+    }
+    return res.status(500).json('internal server error');
+  });
+});
+
+router.get('/', function(req, res) {
+  Language.find({})
+  .then((results) => {
+    if(results) {
+      return res.status(200).json(results);
+    }
+    return res.status(404).json('not found');
+  })
+  .catch((err) => {
+    console.log(err);
+    return res.status(500).json('internal server error');
   });
 });
 
