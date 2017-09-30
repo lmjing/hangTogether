f91bab51bd29104bcda2d28033719ee9081f6f92
lmjing <lmjing1234@gmail.com>
2017-09-30 22:03:01 +0900

    local 작업

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,6 +15,8 @@ var posts = require('./router/Posts');
 app.use('/user', users);
 app.use('/language', languages);
 app.use('/post', posts);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, function() {
   console.log('서버돌아가는 중 ~~');
