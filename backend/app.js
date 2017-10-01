9fc159445e14fe94f0761f8c158e9a27cf61a17f
mijeong lee <lmjing1234@gmail.com>
2017-10-02 03:21:11 +0900

    conflict 해결

--- backend/app.js
+++ backend/app.js
@@ -21,38 +21,11 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(3000, function() {
   console.log('서버돌아가는 중 ~~');
 });
-
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-var promise = mongoose.connect('mongodb://localhost:27017/hangTogether', {
-  useMongoClient: true
-});
-
-var db = mongoose.connection;
-=======
-// var promise = mongoose.connect('mongodb://localhost:27017/hangTogether', {
-//   useMongoClient: true
-// });
-
 mongoose.Promise = global.Promise;
 
-// var db = mongoose.connection;
 var db = mongoose.connect('mongodb://localhost:27017/hangTogether', {
   useMongoClient: true
 });
->>>>>>> Stashed changes
-=======
-// var promise = mongoose.connect('mongodb://localhost:27017/hangTogether', {
-//   useMongoClient: true
-// });
-
-mongoose.Promise = global.Promise;
-
-// var db = mongoose.connection;
-var db = mongoose.connect('mongodb://localhost:27017/hangTogether', {
-  useMongoClient: true
-});
->>>>>>> d24634c134a2223ba5206760384aa1375c4c6eca
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log('connected successfully');
