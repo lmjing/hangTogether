ab82a4d8ab14bc873d8f0674d4c9a1a6c6b1d2d1
mijeong lee <lmjing1234@gmail.com>
2017-10-02 03:22:11 +0900

    conflict 해결

--- backend/model/Language.js
+++ backend/model/Language.js
@@ -4,16 +4,8 @@ var languageSchema = new mongoose.Schema({
     type: String,
     unique: true
   }
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
 },{
     versionKey: false // You should be aware of the outcome after set to false
->>>>>>> Stashed changes
-=======
-},{
-    versionKey: false // You should be aware of the outcome after set to false
->>>>>>> d24634c134a2223ba5206760384aa1375c4c6eca
 });
 
 mongoose.model('Language', languageSchema);
