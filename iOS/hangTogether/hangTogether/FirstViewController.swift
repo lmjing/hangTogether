707133fd9a73daed4b9ddb5c4a2b1b86df0717ed
lmjing <lmjing1234@gmail.com>
2017-10-04 13:38:21 +0900

    mainListCell storyboard 작업 및 아울렛 연결

--- iOS/hangTogether/hangTogether/FirstViewController.swift
+++ iOS/hangTogether/hangTogether/FirstViewController.swift
@@ -2,19 +2,18 @@
 //  FirstViewController.swift
 //  hangTogether
 //
-//  Created by 이미정 on 2017. 9. 28..
+//  Created by 이미정 on 2017. 10. 3..
 //  Copyright © 2017년 이미정. All rights reserved.
 //
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Networking.getLanguages()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.navigationItem.title = "글 목록"
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +21,69 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mainCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
---
+++ iOS/hangTogether/hangTogether/MainTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MainTableViewCell.swift
+//  hangTogether
+//
+//  Created by 이미정 on 2017. 10. 4..
+//  Copyright © 2017년 이미정. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var tripDateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var languageStackView: UIStackView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
