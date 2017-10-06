2ae9d691ca018b21d75b8354c109199bdf7626c9
lmjing <lmjing1234@gmail.com>
2017-10-06 12:39:54 +0900

    스토리보드 레이아웃 1차 구상

--- iOS/hangTogether/hangTogether/WritePostViewController.swift
+++ iOS/hangTogether/hangTogether/WritePostViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class WritePostViewController: UIViewController {
 
+    @IBOutlet weak var tripListViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var tripListView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.tabBarController?.tabBar.isHidden = true
+        
+        tripListView.delegate = self
+        tripListView.dataSource = self
+        tripListViewHeight.constant = 0
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -24,8 +29,6 @@ class WritePostViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
     /*
     // MARK: - Navigation
 
@@ -35,5 +38,15 @@ class WritePostViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension WritePostViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
 }
