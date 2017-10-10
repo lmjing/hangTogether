2c579b68677b38640fb84dfa026979d86a218d72
lmjing <lmjing1234@gmail.com>
2017-10-10 17:48:48 +0900

    data 구조 수정

--- iOS/hangTogether/hangTogether/AddPlaceViewController.swift
+++ iOS/hangTogether/hangTogether/AddPlaceViewController.swift
@@ -13,11 +13,11 @@ class AddPlaceViewController: UIViewController {
     @IBOutlet weak var dateSwitch: UISwitch!
     @IBOutlet weak var placeTextField: UITextField!
     
-    var trip:[String:Any] = {
-        var data:[String:Any] = [:]
-        var place:[String:String] = [:]; data["place"] = place
-        return data
-    }()
+//    var trip:[String:Any] = {
+//        var data:[String:Any] = [:]
+//        var place:[String:String] = [:]; data["place"] = place
+//        return data
+//    }()
     
     var datePicker = UIDatePicker()
     override func viewDidLoad() {
@@ -42,20 +42,24 @@ class AddPlaceViewController: UIViewController {
     }
     
     func done(button: UIBarButtonItem) {
+        var tripdate: String?, placeName: String, placeAddress: String?
         if dateSwitch.isOn, let date = dateTextField.text, date != "" {
-            trip["date"] = date
+//            trip["date"] = date
+            tripdate = date
         }else if !dateSwitch.isOn {
-            trip["date"] = nil
+//            trip["date"] = nil
+            tripdate = nil
         }else {
             let dialog = UIAlertController.okAlert(title: nil, message: "날짜를 선택해주세요.")
             self.present(dialog, animated: true, completion: nil)
             return
         }
 
-        if let place = placeTextField.text, place != "",
-            var data = trip["place"] as? [String:String] {
-            data["name"] = place
-            trip["place"] = data
+        if let place = placeTextField.text, place != "" {
+            placeName = place
+//            var data = trip["place"] as? [String:String] {
+//            data["name"] = place
+//            trip["place"] = data
         }else {
             let dialog = UIAlertController.okAlert(title: nil, message: "장소를 입력해주세요.")
             self.present(dialog, animated: true, completion: nil)
@@ -63,8 +67,40 @@ class AddPlaceViewController: UIViewController {
         }
 
         navigationController?.popViewController(animated: true)
+//        let cv = navigationController?.viewControllers.last as! WritePostViewController
+//        cv.tripList.append(trip)
+        appendTripData(date: tripdate, placeName: placeName, placeAddress: placeAddress)
+    }
+    
+    func appendTripData(date: String?, placeName: String, placeAddress: String?) {
         let cv = navigationController?.viewControllers.last as! WritePostViewController
-        cv.tripList.append(trip)
+        
+        var newPlace: [String:String] = [:]
+        newPlace["name"] = placeName
+        if let address = placeAddress {
+            newPlace["address"] = address
+        }
+        
+        //TODO: notFound가 필요없는지 확인 후 필요없으면 삭제 할 것!
+        var notFound = true
+        print(cv.tripList, "before")
+        cv.tripList = cv.tripList.map { oldtrip -> [String:Any] in
+            if let oldDate = oldtrip["date"] as? String, oldDate == date {
+                if var array = oldtrip["places"] as? [[String:String]] {
+                    array.append(newPlace)
+                    notFound = false
+                }
+            }
+            return oldtrip
+        }
+        
+        if notFound {
+            var newTrip: [String:Any] = [:]
+            newTrip["date"] = date
+            newTrip["places"] = [newPlace]
+            cv.tripList.append(newTrip)
+        }
+        print(cv.tripList, "after")
     }
     
     func pickerDone(button: UIBarButtonItem) {
