f4c6bb3b7e6fa06634370bf44f8a6bf40e64c4fd
lmjing <lmjing1234@gmail.com>
2017-10-15 02:07:04 +0900

    아울렛 ViewController에 연결

--- iOS/hangTogether/hangTogether/DetailPostViewController.swift
+++ iOS/hangTogether/hangTogether/DetailPostViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class DetailPostViewController: UIViewController {
-
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var userInfoLabel: UILabel!
+    @IBOutlet weak var languagesLabel: UILabel!
+    @IBOutlet weak var userProfileView: UIView!
+    
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var endDateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +29,4 @@ class DetailPostViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
