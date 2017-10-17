ae89ce3dd97d89fbf7491a2a5284e1e420360075
lmjing <lmjing1234@gmail.com>
2017-10-17 18:03:51 +0900

    tabViewController 연결

---
+++ iOS/hangTogether/hangTogether/MainTabViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabViewController.swift
+//  hangTogether
+//
+//  Created by 이미정 on 2017. 10. 17..
+//  Copyright © 2017년 이미정. All rights reserved.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if item.tag == 2 || item.tag == 3 {
+            if UserDefaults.standard.object(forKey: "userId") == nil {
+            }
+        }
+    }
+}
