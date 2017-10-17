8041be9e183bf6f8f233660114d759d0ac81c85f
lmjing <lmjing1234@gmail.com>
2017-10-17 23:52:15 +0900

    login page 생성 및 아울렛 연결

---
+++ iOS/hangTogether/hangTogether/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  hangTogether
+//
+//  Created by 이미정 on 2017. 10. 17..
+//  Copyright © 2017년 이미정. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loginButtonAction(sender: AnyObject) {
+    }
+    @IBAction func joinButtonAction(sender: AnyObject) {
+    }
+}
